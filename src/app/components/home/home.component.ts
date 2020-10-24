226847537e87b6456004d967cb8d61cdfdfe12af
yessygc <yessgarciacampos@gmail.com>
2020-10-24 09:06:49 -0500

    error

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -11,14 +11,24 @@ export class HomeComponent {
   nuevasCanciones: any [] = [];
   loading: boolean;
 
+  error: boolean;
+  mensajeError: string;
+
   constructor( private spotify: SpotifyService ) {
 
     this.loading = true;
+    this.error = false;
 
     this.spotify.getNewReleases()
         .subscribe( (data: any) => {
           this.nuevasCanciones = data;
           this.loading = false;
+    }, ( errorServicio ) => {
+
+      this.loading = false;
+      this.error = true;
+      console.log(errorServicio);
+      this.mensajeError = errorServicio.error.error.message;
     });
 
   }
